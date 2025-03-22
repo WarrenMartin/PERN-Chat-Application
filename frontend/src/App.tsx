b98f24bf5391c72c8cda6b355aa3b89907bcac87
WarrenMartin <martinwarren2000@gmail.com>
2025-03-22 15:19:49 +0530

    ContextAPi created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,23 @@
-import { Route,Routes } from "react-router-dom";
+import { Navigate, Route,Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import { useAuthContext } from "./context/AuthContext";
 
 
 function App() {
+  const {authUser,setAuthUser,isLoading} =useAuthContext();
+  console.log("Auth User",authUser)
+
+  if(isLoading) return null;
+
   return (
   
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={authUser?<Home/>:<Navigate to={"/login"}/>} />
+        <Route path="/signup" element={!authUser ? <SignUp /> : <Navigate to="/home" />} />
+        <Route path="/login" element={!authUser ? <Login/> : <Navigate to="/home"/>}/>
       </Routes>
 
     </div>
